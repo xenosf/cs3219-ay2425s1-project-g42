2db4ed2dec3609187a4ba7fb111f9a3cff8e0572
guowei42 <65711401+guowei42@users.noreply.github.com>
2024-09-20 15:39:01 +0800

    add seeded questions

---
+++ server/model/questionModel.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const questionSchema = mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    categories: { type: String, required: true },
+    complexity: { type: String, required: true },
+    link: { type: String, required: true },
+});
