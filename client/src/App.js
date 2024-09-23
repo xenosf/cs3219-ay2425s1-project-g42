9b1523a2244bfd3cf6e9ae1ef899ea9f2bb0abdb
mayuanxin1234 <95178026+mayuanxin1234@users.noreply.github.com>
2024-09-23 11:02:47 +0800

    [FrontEnd] Creation of table to display the questions

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,64 @@
 import React, {useState, useEffect} from "react";
 import './App.css';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import Typography from '@material-ui/core/Typography';
+import axios from 'axios';
 
 function App() {
-  const [message, setMessage] = useState("");
+  const [questionList, setQuestionList] = useState("");
 
-  useEffect(() => {
-    fetch("http://localhost:8000/messages")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message));
-  }, []);
+  useEffect(() => { 
+    const fetchQuestions = async () => {    
+        try {
+          const response = await axios.get("http://localhost:8000/questions");
+          setQuestionList(response.data);
+        } catch(error) {
+          console.error('Error fetching questions', error)
+        }
+      }
+      fetchQuestions()
+  },[questionList])
 
-  console.log(message)
+  let accordionList = [];
+
+  Object.entries(questionList).map((wholeQuestion, index) => {
+      let questionId = parseInt(wholeQuestion[0]) + 1;
+      accordionList.push(<Accordion>
+        <AccordionSummary
+          expandIcon={<ArrowDropDownIcon />}
+          aria-controls="panel1-content"
+          id="panel1-header"
+        >
+          <Typography>{questionId}. {wholeQuestion[1].title}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography align = 'left'>
+            Description: {wholeQuestion[1].description}
+          </Typography>
+          <Typography align = 'left' color = 'secondary'>
+          Category: {wholeQuestion[1].categories}
+          </Typography>
+          <Typography align = 'left' color = 'primary'>
+          Complexity: {wholeQuestion[1].complexity}
+          </Typography>
+          <Typography align = 'left' color = 'primary'>
+            Link:&nbsp; 
+           <a href = {wholeQuestion[1].link}>Click to get to leetcode</a>
+          </Typography>          
+        </AccordionDetails>
+      </Accordion>);
+      return null;
+  });
 
   return (
     <div className="App">
-      <h1>I am working!</h1>
-      <h1>{message}</h1>
-
+      <h1>PeerPrep</h1>
+      <div>
+        {accordionList}
+        </div>      
     </div>
   );
 }
