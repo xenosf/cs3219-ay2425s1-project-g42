345fe1127a1cb1537f0a0b194f3b26ed6522cfb4
Chia Rena <Chocorena@gmail.com>
2024-09-27 22:04:33 +0800

    Added restful API for update

--- server/routes/questionRoute.js
+++ server/routes/questionRoute.js
@@ -69,4 +69,27 @@ router.post("/add", async (req, res) => {
     }
 });
 
+// update question
+
+router.put("/:id", async(req, res) => {
+    const {id} = req.params;
+    const updates = req.body;
+
+    try {
+        const updatedQuestion = await Question.findByIdAndUpdate(
+            id,
+            update,
+            { new: true, runValidators: true }
+        );
+
+        if(!updatedQuestion) {
+            return res.status(404).json({message: "Question not found"})
+        }
+
+        res.status(200).json(updatedQuestion);
+    } catch (error) {
+        res.status(500).json({ message: error.message});
+    }
+})
+
 module.exports = router;
