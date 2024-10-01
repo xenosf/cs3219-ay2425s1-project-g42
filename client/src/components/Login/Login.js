3b3f9f1c6f4f0bb1187995e82fc0277284299d10
guowei42 <65711401+guowei42@users.noreply.github.com>
2024-10-01 23:42:34 +0800

    add login input ui and buttons

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,7 +1,48 @@
+import {
+  Box,
+  Button,
+  Container,
+  Link,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import { ReactComponent as Logo } from "./assets/logo.svg";
+import { ReactComponent as Background } from "./assets/background.svg";
+import "./style/login.css";
+import { red } from "@mui/material/colors";
+
 function Login() {
   return (
-    <div>This is the login screen!</div>
-  )
+    <Container id="login-container">
+      <Box id="login-box">
+        <Box sx={{ fontSize: "2rem", fontWeight: "bold", fontWeight: 500 }}>
+          LOGIN
+        </Box>
+        <TextField label="Email" className="login-input" />
+        <TextField label="Password" className="login-input" />
+        <Box id="login-footer">
+          <Link component={RouterLink} to="/">
+            Forgot your password?
+          </Link>
+          <RouterLink>
+            <Button variant="contained" id="login-button">
+              Login
+            </Button>
+          </RouterLink>
+          <Typography variant="body2" id="login-to-register">
+            Don't have an account?
+            <Typography variant="body2">
+              Sign up{" "}
+              <Link component={RouterLink} to="/">
+                here
+              </Link>
+            </Typography>
+          </Typography>
+        </Box>
+      </Box>
+    </Container>
+  );
 }
 
 export default Login;
